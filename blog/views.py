406d74def17a984be50545d222d7f458cc6e78d0
sakasegawayosuke <sy1998mtw@gmail.com>
2020-02-06 22:00:41 +0900

    Add viewsto create/edit blog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+
 
 # Create your views here.
 
@@ -12,3 +15,38 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+#-------------------------------------------------Postフォームを新しく作るには、PostForm()を呼び出し、テンプレートに出力
+def post_new(request):
+    if request.method == "POST":    # 新しく投稿された時
+        form = PostForm(request.POST)   
+
+        # フォームの値が正しいかどうか
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()     #フォームの値が有効なら保存
+            return redirect('post_detail', pk=post.pk)      #post_detail.htmlに移動
+
+    else :  #投稿がなければフォームを新しく作る
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+#--------------------------------------------------- 投稿したものを編集する
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
